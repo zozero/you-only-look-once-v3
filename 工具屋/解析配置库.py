b0ef88c2638381bbd55f7767f4a5bda4fac29828
zozero <929096507@qq.com>
2023-03-11 08:01:22 +0800

    继续完善参数代码

--- 工具屋/解析配置库.py
+++ 工具屋/解析配置库.py
@@ -2,7 +2,7 @@ def 解析数据配置(路径):
     选项字典 = dict()
     选项字典['多个图形处理单元'] = '0,1,2,3'
     选项字典['线程数'] = '10'
-    with open(路径, 'r') as 文件:
+    with open(路径, 'r',encoding='utf8') as 文件:
         多行 = 文件.readlines()
     for 行 in 多行:
         行 = 行.strip()
--- 训练.py
+++ 训练.py
@@ -2,6 +2,7 @@ import os
 
 import torch
 
+from 工具屋.工具库 import 载入分类列表
 from 工具屋.解析配置库 import 解析数据配置
 from 工具屋.记录器 import 记录器
 from 配置屋.配置 import 参数
@@ -9,9 +10,12 @@ from 配置屋.配置 import 参数
 if __name__ == '__main__':
     记录者 = 记录器("日志房")
 
-    设备=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    设备 = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    os.makedirs("输出间",exist_ok=True)
-    os.makedirs("检查点居室",exist_ok=True)
+    os.makedirs("输出间", exist_ok=True)
+    os.makedirs("检查点居室", exist_ok=True)
 
-    数据配置=解析数据配置(参数.数据配置_文件路径)
+    数据配置 = 解析数据配置(参数.数据配置_文件路径)
+    训练_路径 = 数据配置["训练"]
+    验证_路径 = 数据配置["验证"]
+    分类名称列表 = 载入分类列表(数据配置["名称列表"])
