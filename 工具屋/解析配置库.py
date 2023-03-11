ae5635a52b13e547029a75c2332568d216857221
zozero <929096507@qq.com>
2023-03-11 16:23:29 +0800

    完成部分网络模型代码

--- 工具屋/解析配置库.py
+++ 工具屋/解析配置库.py
@@ -2,7 +2,7 @@ def 解析数据配置(路径):
     选项字典 = dict()
     选项字典['多个图形处理单元'] = '0,1,2,3'
     选项字典['线程数'] = '10'
-    with open(路径, 'r',encoding='utf8') as 文件:
+    with open(路径, 'r', encoding='utf8') as 文件:
         多行 = 文件.readlines()
     for 行 in 多行:
         行 = 行.strip()
@@ -12,3 +12,23 @@ def 解析数据配置(路径):
         选项字典[键.strip()] = 值.strip()
 
     return 选项字典
+
+
+def 解析模型配置(路径):
+    文件 = open(路径, 'r')
+    多行 = 文件.read().split('\n')
+    多行 = [x for x in 多行 if x and not x.startswith('#')]
+    多行 = [x.rstrip().lstrip() for x in 多行]
+    模块定义列表 = []
+    for 行 in 多行:
+        if 行.startswith('['):
+            模块定义列表.append({})
+            模块定义列表[-1]['类型'] = 行[1:-1].rstrip()
+            if 模块定义列表[-1]['类型'] == '卷积':
+                模块定义列表[-1]['每批归一化'] = 0
+        else:
+            键, 值 = 行.split('=')
+            值 = 值.strip()
+            模块定义列表[-1][键.rstrip()] = 值.strip()
+
+    return 模块定义列表
--- 配置屋/配置.py
+++ 配置屋/配置.py
@@ -4,7 +4,7 @@ import argparse
 解析器.add_argument("--轮回数", type=int, default=100, help="轮回的次数")
 解析器.add_argument("--单批数", type=int, default=4, help="一批图片的张数")
 解析器.add_argument("--梯度累积", type=int, default=2, help="每个步骤前的梯度累积数")
-解析器.add_argument("--模型配置_文件路径", type=str, default="配置屋/我只看一次版本3.配置", help="模型配置文件路径")
+解析器.add_argument("--模型定义_文件路径", type=str, default="配置屋/我只看一次版本3.配置", help="模型配置文件路径")
 解析器.add_argument("--数据配置_文件路径", type=str, default="配置屋/coco.数据", help="数据配置文件路径")
 解析器.add_argument("--预训练权重_文件路径", type=str, help="如果使用检查点内的模型的话")
 解析器.add_argument("--中央处理器的线程数", type=int, default=0, help="计算单批数据期间使用中央处理器的线程数量")
